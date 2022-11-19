4f3501cb3148cce028daa5bd7a53a6a44a22dd23
Nathan Araujo <nathan.araujo@stu.ocsb.ca>
2022-11-19 02:35:25 +0000

    Fixed more errors

--- Assign-04-C++.cpp
+++ Assign-04-C++.cpp
@@ -45,46 +45,45 @@ int main() {
     try {
         userNum1Int = std::stoi(userNum1Str);
         userNum2Int = std::stoi(userNum2Str);
+        // If statement to check if totalInt is a invalid input
+        if (userNum1Int > 0 && userNum2Int > 0) {
+            if (userNum1Int > userNum2Int) {
+                greatestNum = userNum1Int;
+            } else {
+                greatestNum = userNum2Int;
+            }
+            // for loop to calculate all the products of the
+            // from the counter to the greatestNum using userNum2Int
+            for (int counter2 = 1; counter2 <= greatestNum; counter2++) {
+                // Asks the user for a number to add to sum
+                product2 = userNum2Int * counter2;
+                product2List.push_back(product2);
+                product2List.sort();
+                // for loop to calculate all the products of the
+                // from the counter to the greatestNum using userNum1Int
+                for (int counter1 = 1; counter1 <= greatestNum; counter1++) {
+                    product = userNum1Int * counter1;
+                    product1List.push_back(product);
+                    product1List.sort();
+                }
+                // To check if product2 is in product1List
+                bool findListProduct = (std::find(product1List.begin(),
+                product1List.end(), product2) != product1List.end());
+                // if it is in product1List then add product2 to commonList
+                if (findListProduct == true) {
+                    commonList.push_back(product2);
+                } else {
+                    continue;
+                }
+            }
+            std::cout << "If nothing is appearing wait for it to load\n";
+            // Displays the first number of commonList
+            std::cout << "The Lowest Common Multiple of " << userNum2Int << " and "
+            << userNum1Int << " is "<< commonList.front() << "\n";
+        } else {
+            std::cout << "Enter a positive integer\n";
+        }
     } catch (const std::exception) {
         std::cout << "Please a valid integer\n";
-    }
-
-    // If statement to check if totalInt is a invalid input
-    if (userNum1Int > 0 && userNum2Int > 0) {
-        if (userNum1Int > userNum2Int) {
-            greatestNum = userNum1Int;
-        } else {
-            greatestNum = userNum2Int;
-        }
-        // for loop to calculate all the products of the
-        // from the counter to the greatestNum using userNum2Int
-        for (int counter2 = 1; counter2 <= greatestNum; counter2++) {
-            // Asks the user for a number to add to sum
-            product2 = userNum2Int * counter2;
-            product2List.push_back(product2);
-            product2List.sort();
-            // for loop to calculate all the products of the
-            // from the counter to the greatestNum using userNum1Int
-            for (int counter1 = 1; counter1 <= greatestNum; counter1++) {
-                product = userNum1Int * counter1;
-                product1List.push_back(product);
-                product1List.sort();
-            }
-            // To check if product2 is in product1List
-            bool findListProduct = (std::find(product1List.begin(),
-            product1List.end(), product2) != product1List.end());
-            // if it is in product1List then add product2 to commonList
-            if (findListProduct == true) {
-                commonList.push_back(product2);
-            } else {
-                continue;
-            }
-        }
-        std::cout << "If nothing is appearing wait for it to load\n";
-        // Displays the first number of commonList
-        std::cout << "The Lowest Common Multiple of " << userNum2Int << " and "
-        << userNum1Int << " is "<< commonList.front() << "\n";
-    } else {
-        std::cout << "Enter a positive integer\n";
     }
 }
